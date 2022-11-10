c60a182ae2a33d80cad8172423a477a6ffca42be
Cargomillieux <116322048+Cargomillieux@users.noreply.github.com>
2022-11-10 09:31:10 -0500

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ servos.P0.set_range(10, 170)
 
 def on_forever():
     global angle
-    for index in range(11):
-        angle = (0 + 0) * 0
+    for index in range(17):
+        angle = index * 10 + 10
         servos.P0.set_angle(angle)
         basic.pause(100)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -2,8 +2,8 @@ let angle = 0
 servos.P0.setRange(10, 170)
 basic.forever(function on_forever() {
     
-    for (let index = 0; index < 11; index++) {
-        angle = (0 + 0) * 0
+    for (let index = 0; index < 17; index++) {
+        angle = index * 10 + 10
         servos.P0.setAngle(angle)
         basic.pause(100)
     }
